43df797171f12e1983286922391afc032e066604
Joshua Lynch <jklynch@email.arizona.edu>
2017-11-07 10:44:42 -0500

    add direct loading feature

--- loaders/uproc_results/kegg/load_kegg_results_to_uproc_kegg_table.py
+++ loaders/uproc_results/kegg/load_kegg_results_to_uproc_kegg_table.py
@@ -39,6 +39,9 @@ def get_args():
     argparser.add_argument('--results-root-dp',
                            help='path to root of results directory tree')
 
+    argparser.add_argument('--load-results-root-dp',
+                           help='path to root of results directory tree')
+
     argparser.add_argument('--line-limit',
                            default=None,
                            type=int,
@@ -76,6 +79,12 @@ def main():
         ##SampleToUproc.__table__.create(imicrobe_engine)
         #load_sample_to_uproc_table(session=session, engine=imicrobe_engine)
         write_command_file_from_directory_tree(dir_root=args.results_root_dp,)
+    elif args.load_results_root_dp:
+        load_all_samples_to_uproc_kegg_table_from_directory_tree(
+            dir_root=args.load_results_root_dp,
+            session=session,
+            engine=imicrobe_engine,
+            line_limit=args.line_limit)
     elif args.uproc_results_fp:
         load_sample_to_uproc_table_from_file(
             uproc_results_fp=args.uproc_results_fp,
@@ -132,6 +141,133 @@ def write_command_file_from_directory_tree(dir_root):
                 print('python kegg/load_kegg_results_to_uproc_kegg_table.py --uproc-results-fp {}'.format(uproc_results_fp))
 
     sys.stderr.write('wrote {} lines in {:5.1f}s\n'.format(file_count, time.time()-start_time))
+
+
+def load_all_samples_to_uproc_kegg_table_from_directory_tree(dir_root, session, engine, line_limit):
+    from loaders.uproc_results.kegg.models import Kegg_annotation, Uproc_kegg_result
+
+    # load the kegg_annotations table first
+    start_time = time.time()
+    t0 = time.time()
+    file_count = 0
+    downloaded_kegg_annotations = set()
+    download_failed_kegg_ids = set()
+    for root, dirs, files in os.walk(dir_root):
+        for file_name in files:
+            if file_name.endswith('.uproc.kegg'):
+                file_count += 1
+                uproc_kegg_results_fp = os.path.join(root, file_name)
+                with open(uproc_kegg_results_fp, 'rt') as uproc_kegg_results_file:
+
+                    # UProC results files look like this:
+                    #   K01467,4208
+                    #   K01990,660
+                    #   K07481,434
+                    #   ... and so on ...
+                    t00 = time.time()
+                    file_line_count = 0
+                    file_downloaded_kegg_annotations = []
+                    for line in take(line_limit, uproc_kegg_results_file):
+                        kegg_id, read_count = line.strip().split(',')
+                        file_line_count += 1
+
+                        if kegg_id in downloaded_kegg_annotations:
+                            # no need to check the database for this kegg annotation
+                            #print('KEGG id {} has already been downloaded'.format(kegg_id))
+                            pass
+                        elif kegg_id in download_failed_kegg_ids:
+                            print('KEGG id {} can not be downloaded')
+                        else:
+                            kegg_annotation = session.query(
+                                Kegg_annotation).filter(
+                                    Kegg_annotation.kegg_annotation_id == kegg_id).one_or_none()
+                            if kegg_annotation is not None:
+                                # no need to insert this kegg_id in the kegg_annotations table
+                                # why was it not in downloaded_kegg_annotations?
+                                pass
+                            else:
+                                # download and insert
+                                #print('failed to find KEGG id "{}"'.format(kegg_id))
+                                #print('  downloading KEGG annotation {}'.format(len(downloaded_kegg_annotations) + 1))
+
+                                kegg_annotation_response = requests.get('http://rest.kegg.jp/get/ko:{}'.format(kegg_id))
+                                if kegg_annotation_response.status_code == 200:
+                                    name, definition, pathway, module = parse_kegg_response(
+                                        kegg_annotation_response.text)
+                                    session.add(
+                                        Kegg_annotation(
+                                            kegg_annotation_id=kegg_id,
+                                            name=name,
+                                            definition=definition,
+                                            pathway=pathway,
+                                            module=module))
+
+                                    file_downloaded_kegg_annotations.append(kegg_id)
+                                    downloaded_kegg_annotations.add(kegg_id)
+
+                                else:
+                                    print('  DOWNLOAD FAILED for "{}"'.format(kegg_id))
+                                    download_failed_kegg_ids.add(kegg_id)
+                    print('{:<10.1f}s: parsed {} line(s) of file {}: "{}" in {:5.1f}s'.format(
+                        time.time()-t0, file_line_count, file_count, file_name, time.time()-t00))
+                    print('  downloaded {} of {} KEGG ids'.format(
+                        len(file_downloaded_kegg_annotations), file_line_count))
+    # commit all kegg annotations at the end
+    session.commit()
+
+    print('downloaded and inserted {} KEGG annotations in {:5.1f}s\n'.format(
+        len(downloaded_kegg_annotations), time.time()-start_time))
+
+    t0 = time.time()
+    uproc_kegg_results_files = []
+    # load the uproc_kegg_results table last
+    for root, dirs, files in os.walk(dir_root):
+        for file_name in files:
+            if file_name.endswith('.uproc.kegg'):
+                file_count += 1
+                uproc_kegg_results_fp = os.path.join(root, file_name)
+                # uproc_kegg_results_fp looks like
+                #   /home/u26/jklynch/usr/local/imicrobe/data/uproc/projects/148/samples/3486/ERR906934.fasta.uproc.kegg
+                # get the sample id from the last directory name
+                _, sample_id = os.path.split(os.path.dirname(uproc_kegg_results_fp))
+                sample_id = int(sample_id)
+
+                t00 = time.time()
+                file_results_count = 0
+                with open(uproc_kegg_results_fp, 'rt') as uproc_kegg_results_file:
+
+                    # UProC results files look like this:
+                    #   K01467,4208
+                    #   K01990,660
+                    #   K07481,434
+                    #   ... and so on ...
+                    for line in take(line_limit, uproc_kegg_results_file):
+                        kegg_id, read_count = line.strip().split(',')
+                        if kegg_id in download_failed_kegg_ids:
+                            pass
+                        else:
+                            session.add(
+                                Uproc_kegg_result(
+                                    sample_id=sample_id,
+                                    kegg_annotation_id=kegg_id,
+                                    read_count=int(read_count)))
+                            file_results_count += 1
+
+                    uproc_kegg_results_files.append(file_name)
+
+                print('finished file {}: "{}" with {} results in {:5.1f}s'.format(
+                    len(uproc_kegg_results_files), file_name, file_results_count, time.time()-t00))
+
+        # commit after parsing each file
+        session.commit()
+
+    print('downloaded and inserted {} UProC KEGG results in {:5.1f}s\n'.format(
+        len(downloaded_kegg_annotations), time.time()-t0))
+
+    print('failed to download {} annotation(s):\n\t{}'.format(
+        len(download_failed_kegg_ids), '\n\t'.join(download_failed_kegg_ids)))
+
+    print('total time: {:5.1f}s'.format(time.time()-start_time))
 
 
 def load_sample_to_uproc_table_from_file(uproc_results_fp, session, engine):
@@ -199,6 +335,7 @@ def load_sample_to_uproc_table_from_file(uproc_results_fp, session, engine):
                 Uproc_kegg_result.__tablename__,
                 time.time() - t0))
 
+
 name_re = re.compile(r'^NAME(\s+)(?P<name>.+)$')
 definition_re = re.compile(r'^DEFINITION(\s+)(?P<definition>.+)$')
 pathway_re = re.compile(r'^(PATHWAY)?(\s+)(?P<pathway>.+)$')
@@ -213,14 +350,17 @@ def parse_kegg_response(response):
                       ko02020  Two-component system
           MODULE      M00628  beta-Lactam resistance, AmpC system
     """
+
+    debug = False
+
     response_buffer = io.StringIO(response)
     entry_line = response_buffer.readline()
     name_line = response_buffer.readline()
     name = name_re.search(name_line).group('name')
-    print('  name: "{}"'.format(name))
+    if debug: print('  name: "{}"'.format(name))
     definition_line = response_buffer.readline()
     definition = definition_re.search(definition_line).group('definition')
-    print('  definition: "{}"'.format(definition))
+    if debug: print('  definition: "{}"'.format(definition))
 
     pathway = []
     module = []
@@ -241,7 +381,7 @@ def parse_kegg_response(response):
         else:
             pass
 
-        print(line)
+        if debug: print(line)
         if parsing_section == 'pathway':
             pathway.append(pathway_re.search(line).group('pathway'))
         elif parsing_section == 'module':
@@ -252,9 +392,9 @@ def parse_kegg_response(response):
 
     pathway = '\n'.join(pathway)
     module = '\n'.join(module)
-    
-    print('  pathway: "{}"'.format(pathway))
-    print('  module: "{}"'.format(module))
+
+    if debug: print('  pathway: "{}"'.format(pathway))
+    if debug: print('  module: "{}"'.format(module))
 
     return name, definition, pathway, module
 
