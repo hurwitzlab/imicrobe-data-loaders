3638ed0628f664098f6679542ab50dc4b0b9c83a
Joshua Lynch <jklynch@email.arizona.edu>
2017-12-08 15:44:08 -0500

    reorganize directories add more irods support

---
+++ loader/imicrobe/hl2a/delong/load.py
@@ -0,0 +1,196 @@
+"""
+Copy HL2A DeLong data from muSCOPE to iMicrobe.
+
+Copy samples that look like these:
+
+ CSHLIID00-20a-S06C001-0015
+ CSHLIIR00-20a-S06C001-0015
+ CSHLIID20-03a-S06C001-0015
+
+In the iMicrobe database a sample has 'sample_acc' and 'sample_name'.
+The 'sample_acc' will be 'CSHLIID00-20a' and 'sample_name' will be
+'CSHLIID00-20a-S06C001-0015'.
+
+Find these samples in the muSCOPE database
+Create corresponding entries in the iMicrobe database.
+Copy data files from
+    /iplant/home/scope/data/delong/HL2A
+to
+    /iplant/home/shared/imicrobe/projects/266
+"""
+import os
+
+import loader.imicrobe.models as im
+from loader.util.irods import \
+    irods_copy, irods_create_collection, irods_delete, irods_delete_collection, irods_session_manager
+import muscope_loader.models as mu
+from orminator import session_manager_from_db_uri
+
+
+delete_imicrobe_samples = False
+copy_files = True
+hl2a_delong_project_id = 266
+
+with session_manager_from_db_uri(db_uri=os.environ.get('MUSCOPE_DB_URI')) as mu_session:
+
+    mu_samples = mu_session.query(mu.Sample).filter(
+        mu.Sample.sample_name.like(
+            'CSHLII%%0-%%a-S%%C%%%-0015')).all()
+
+    print('found {} results'.format(len(mu_samples)))
+    for mu_sample in mu_samples:
+        with session_manager_from_db_uri(db_uri=os.environ.get('IMICROBE_DB_URI')) as im_session:
+            print('\n{}'.format(mu_sample.sample_name))
+
+            im_sample = im_session.query(im.Sample).filter(
+                im.Sample.project_id == hl2a_delong_project_id).filter(
+                im.Sample.sample_acc == mu_sample.sample_name).one_or_none()
+
+            if im_sample is None:
+                print('sample "{}" does not exist in imicrobe database'.format(mu_sample.sample_name))
+            else:
+                print('found sample "{}" in imicrobe database'.format(im_sample.sample_name))
+                if delete_imicrobe_samples:
+                    print('  deleting sample "{}" from imicrobe database'.format(im_sample.sample_name))
+                    im_session.delete(im_sample)
+                    # force the delete or things go bad when the new sample is added
+                    im_session.flush()
+                    im_sample = None
+                else:
+                    print('  no change to sample "{}"'.format(im_sample.sample_name))
+
+            if im_sample is None:
+                print('creating sample "{}" in imicrobe database'.format(mu_sample.sample_name))
+                im_sample = im.Sample(
+                    project_id=hl2a_delong_project_id,
+                    sample_acc=mu_sample.sample_name,
+                    sample_name=mu_sample.sample_name,
+                    sample_type='archaea,bacteria,virus',
+                    latitude=mu_sample.latitude_start,
+                    longitude=mu_sample.longitude_start,
+                    taxon_id=0,
+                    url='none')
+                im_session.add(im_sample)
+            else:
+                print('sample "{}" unchanged in imicrobe database'.format(im_sample.sample_name))
+
+            # need sample_id to find attributes and derive sample file path
+            im_sample_id = im_session.query(im.Sample.sample_id).filter(
+                im.Sample.project_id == hl2a_delong_project_id).filter(
+                im.Sample.sample_acc == mu_sample.sample_name).one()[0]
+            print('im_sample_id: {}'.format(im_sample_id))
+
+            # copy attributes from muscope sample to imicrobe sample
+            # if they do not already exist
+            # if attributes do not match update the imicrobe attribute to match the muscope attribute
+            for mu_sample_attr in mu_sample.sample_attr_list:
+                im_sample_attr_type = im_session.query(im.Sample_attr_type).filter(
+                    im.Sample_attr_type.type_ == mu_sample_attr.sample_attr_type.type_).one_or_none()
+                if im_sample_attr_type is None:
+                    print('sample attribute type "{}" does not exist in imicrobe'.format(
+                        mu_sample_attr.sample_attr_type.type_))
+                else:
+                    print('  setting imicrobe sample attribute with type "{}" to "{}"'.format(
+                        mu_sample_attr.sample_attr_type.type_, mu_sample_attr.value))
+
+                    im_sample_attr = im_session.query(im.Sample_attr).filter(
+                        im.Sample_attr.sample == im_sample).filter(
+                        im.Sample_attr.sample_attr_type == im_sample_attr_type).one_or_none()
+                    if im_sample_attr is None:
+                        print('  creating imicrobe sample attribute with type "{}" and value "{}" for sample "{}"'.format(
+                            mu_sample_attr.sample_attr_type.type_,
+                            mu_sample_attr.value,
+                            im_sample.sample_name))
+                        im_sample_attr = im.Sample_attr()
+                        im_sample_attr.sample = im_sample
+                        im_sample_attr.sample_attr_type = im_sample_attr_type
+                        im_sample_attr.attr_value = mu_sample_attr.value
+                    elif im_sample_attr.attr_value == mu_sample_attr.value:
+                        print('  imicrobe sample "{}" has attribute "{}" with value "{}" matching muscope sample attribute value "{}"'.format(
+                            im_sample.sample_name,
+                            im_sample_attr.sample_attr_type.type_,
+                            im_sample_attr.attr_value,
+                            mu_sample_attr.value
+                        ))
+                    else:
+                        print('  imicrobe sample "{}" has attribute "{}" with value "{}" NOT matching muscope sample attribute value "{}"'.format(
+                            im_sample.sample_name,
+                            im_sample_attr.sample_attr_type.type_,
+                            im_sample_attr.attr_value,
+                            mu_sample_attr.value
+                        ))
+                        print('    imicrobe sample attribute will be updated to match muscope attribute')
+                        im_sample_attr.attr_value = mu_sample_attr.value
+
+            # copy sample files from muscope to imicrobe
+            irods_delete_collection_set = set()
+            im_existing_sample_files = im_session.query(im.Sample_file).filter(
+                im.Sample_file.sample_id == im_sample_id).all()
+            if len(im_existing_sample_files) > 0:
+                print('  removing sample files from irods')
+                with irods_session_manager() as irods_session:
+                    for im_existing_sample_file in im_existing_sample_files:
+                        print('    deleting "{}"'.format(im_existing_sample_file.file_))
+                        irods_delete(irods_session, im_existing_sample_file.file_)
+
+                        parent, child = os.path.split(im_existing_sample_file.file_)
+                        irods_delete_collection_set.add(parent)
+                    for irods_collection in irods_delete_collection_set:
+                        print('    deleting collection "{}"'.format(irods_collection))
+                        irods_delete_collection(irods_session, irods_collection)
+            else:
+                pass
+
+            # copy files
+            im_sample_file_type_reads = im_session.query(im.Sample_file_type).filter(
+                im.Sample_file_type.type_ == 'Reads').one()
+            for mu_sample_file in mu_sample.sample_file_list:
+                # at this time only reads will be copied
+                if mu_sample_file.sample_file_type.type_ == 'Reads' \
+                        and (mu_sample_file.file_.endswith('_001.fastq') or
+                             mu_sample_file.file_.endswith('readpool.fastq.gz')):
+
+                    im_sample_collection_path = '/iplant/home/shared/imicrobe/projects/{}/sample/{}'.format(
+                        hl2a_delong_project_id,
+                        im_sample_id)
+                    im_sample_file_path = '{}/{}'.format(
+                        im_sample_collection_path,
+                        os.path.basename(mu_sample_file.file_))
+
+                    # is im_sample already associated with the imicrobe version of this file?
+                    mu_sample_file_basename = os.path.basename(mu_sample_file.file_)
+                    print('searching for imicrobe sample file with basename "{}"'.format(mu_sample_file_basename))
+                    im_sample_file = im_session.query(im.Sample_file).filter(
+                        im.Sample_file.sample == im_sample).filter(
+                        im.Sample_file.file_.like('%{}'.format(mu_sample_file_basename))).one_or_none()
+
+                    if im_sample_file is None:
+                        print('  creating imicrobe sample file "{}"'.format(im_sample_file_path))
+
+                        im_sample_file = im.Sample_file(file_=im_sample_file_path)
+                        im_sample_file.sample = im_sample
+                        im_sample_file.sample_file_type = im_sample_file_type_reads
+                    else:
+                        print('  imicrobe sample file "{}" already exists'.format(im_sample_file_path))
+
+                    print('  + copying file "{}"'.format(mu_sample_file.file_))
+                    print('    to file "{}"'.format(im_sample_file_path))
+
+                    if copy_files:
+                        with irods_session_manager() as irods_session:
+                            print('creating collection "{}"'.format(im_sample_collection_path))
+                            irods_create_collection(
+                                irods_session,
+                                target_collection_path=im_sample_collection_path)
+                            print('copying')
+                            irods_copy(
+                                irods_session,
+                                src_path=mu_sample_file.file_,
+                                dest_path=im_sample_file.file_)
+                    else:
+                        print('*** file copy is disabled ***')
+
+                else:
+                    print('  - ignoring file "{}"'.format(mu_sample_file.file_))
+
+        break
---
+++ loader/util/irods.py
@@ -0,0 +1,54 @@
+import os
+
+from irods.keywords import FORCE_FLAG_KW
+from irods.session import iRODSSession
+from irods.exception import CAT_NO_ROWS_FOUND, CollectionDoesNotExist
+
+
+def irods_session_manager():
+    return iRODSSession(irods_env_file=os.path.expanduser('~/.irods/irods_environment.json'))
+
+
+def irods_create_collection(irods_session, target_collection_path):
+    """Create the specified collection and all parent collections
+    that do not exist.
+
+    :param irods_session:
+    :param target_collection_path:
+    :return:
+    """
+    collection_list = []
+    parent = target_collection_path
+    child = 'just to get started'
+    while len(child) > 0:
+        try:
+            irods_session.collections.get(parent)
+            break
+        except CollectionDoesNotExist:
+            collection_list.insert(0, parent)
+            parent, child = os.path.split(parent)
+            print('parent: {}'.format(parent))
+            print('child : {}'.format(child))
+
+    while len(collection_list) > 0:
+        collection_path = collection_list.pop(0)
+        print('creating collection "{}"'.format(collection_path))
+        irods_session.collections.create(collection_path)
+
+
+def irods_copy(irods_session, src_path, dest_path):
+    irods_session.data_objects.copy(src_path=src_path, dest_path=dest_path, **{FORCE_FLAG_KW: True})
+
+
+def irods_delete(irods_session, target_path):
+    try:
+        irods_session.data_objects.unlink(path=target_path, force=True)
+    except CAT_NO_ROWS_FOUND:
+        print('unable to delete data_object "{}" because it does not exist'.format(target_path))
+
+
+def irods_delete_collection(irods_session, target_collection_path):
+    try:
+        irods_session.collections.remove(target_collection_path)
+    except CAT_NO_ROWS_FOUND:
+        print('unable to delete collection "{}" because it does not exist'.format(target_collection_path))
