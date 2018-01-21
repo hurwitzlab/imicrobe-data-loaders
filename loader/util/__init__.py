84e3f7fbe79d663c6786bf28f12c550c517618bd
Joshua Lynch <jklynch@email.arizona.edu>
2018-01-21 13:12:24 -0500

    add take() from itertools recipes

--- loader/util/__init__.py
+++ loader/util/__init__.py
@@ -1,4 +1,4 @@
-from itertools import zip_longest
+from itertools import islice, zip_longest
 
 
 def grouper(iterable, n, fillvalue=None):
@@ -6,3 +6,7 @@ def grouper(iterable, n, fillvalue=None):
     # grouper('ABCDEFG', 3, 'x') --> ABC DEF Gxx"
     args = [iter(iterable)] * n
     return zip_longest(*args, fillvalue=fillvalue)
+
+def take(n, iterable):
+    "Return first n items of the iterable as a list"
+    return list(islice(iterable, n))
