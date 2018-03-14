a627d5c174bb98122cdf3114566ed048b58e93b5
Joshua Lynch <jklynch@email.arizona.edu>
2018-03-14 07:07:06 -0400

    do not list good files just print the count print cause of failure for each bad file use sets to check for bad letters

--- imicrobe/validate/fasta/fasta_validator.py
+++ imicrobe/validate/fasta/fasta_validator.py
@@ -11,8 +11,8 @@ from Bio.Alphabet import IUPAC
 
 def get_args(argv):
     arg_parser = argparse.ArgumentParser()
-    arg_parser.add_argument('--fasta-glob', required=True, help='glob for FASTA files to be validated')
-    arg_parser.add_argument('--max-workers', required=True, type=int, help='number of processes')
+    arg_parser.add_argument('-i', '--fasta-glob', required=True, help='glob for FASTA files to be validated')
+    arg_parser.add_argument('--max-workers', type=int, default=1, help='number of processes')
 
     args = arg_parser.parse_args(argv)
     print('command line arguments:\n\t{}'.format(args))
@@ -25,10 +25,8 @@ def main():
 
 
 def fasta_validate(fasta_glob, max_workers):
-    print('glob: "{}"'.format(fasta_glob))
-
     fasta_list = glob.glob(fasta_glob, recursive=True)
-    print('  found {} files'.format(len(fasta_list)))
+    print('glob "{}" matched {} files'.format(fasta_glob, len(fasta_list)))
 
     good = []
     bad = []
@@ -39,34 +37,40 @@ def fasta_validate(fasta_glob, max_workers):
             try:
                 read_count, t = future.result()
             except Exception as exc:
-                bad.append(fasta_fp)
+                bad.append((fasta_fp, exc))
             else:
                 good.append((fasta_fp, read_count, t))
 
-        print('problematic files:')
-        for i, fp in enumerate(sorted(bad)):
-            print('\t{}: {}'.format(i+1, fp))
+        print('\n{} valid FASTA file(s)\n'.format(len(good)))
 
-        #print('good files:')
-        #for i, (fp, read_count, t) in enumerate(sorted(good)):
-        #    print('\t{}: {} {} read(s) in {:5.2f}s'.format(i+1, fp, read_count, t))
+        sorted_bad = sorted(bad)
+        print('{} problematic file(s):'.format(len(bad)))
+        print('\n'.join([b[0] for b in sorted_bad]))
+
+        print('\nfailures:')
+        print('\n\n'.join([str(b[1]) for b in sorted_bad]))
 
 
 def parse_fasta(fasta_fp):
     t0 = time.time()
+    alphabet = set(IUPAC.ambiguous_dna.letters)
     read_count = 0
     for record in SeqIO.parse(fasta_fp, format='fasta', alphabet=IUPAC.ambiguous_dna):
-        if Alphabet._verify_alphabet(record.seq):
+        read_count += 1
+        seq_letters = set(str(record.seq))
+        if len(seq_letters.difference(alphabet)) == 0:
             pass
         else:
-            raise Exception()
+            msg ='{}: Failed to parse sequence {}\nid: {}\nsequence: {}'.format(fasta_fp, read_count, record.id, record.seq)
+            raise Exception(msg)
 
         if len(record.seq) == 0:
-            raise Exception()
-        read_count += 1
+            msg = '{}: Record {} has 0-length sequence\nid: {}'.format(fasta_fp, read_count, record.id)
+            raise Exception(msg)
 
     if read_count == 0:
-        raise Exception()
+        msg = '{} is empty'.format(fasta_fp)
+        raise Exception(msg)
 
     t = time.time() - t0
     return read_count, t
