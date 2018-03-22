c92db819972d9c6e13ae46c4c77b3223079c1e99
Joshua Lynch <jklynch@email.arizona.edu>
2018-03-22 15:01:47 -0400

    open a new irods session for each group of 100 files

--- imicrobe/load/sample_file_table/sync_with_iplant_data_store.py
+++ imicrobe/load/sample_file_table/sync_with_iplant_data_store.py
@@ -1,20 +1,21 @@
 import os
 
 import imicrobe.models as models
+import imicrobe.util as util
 import imicrobe.util.irods as irods
 from orminator import session_manager_from_db_uri
 
 
 # check every row in table sample_file
-
+sample_file_i = 0
 with session_manager_from_db_uri(db_uri=os.environ['IMICROBE_DB_URI']) as imicrobe_db_session:
-    for sample_file in imicrobe_db_session.query(models.Sample_file).limit(10):
-        print(dir(sample_file))
-        print(sample_file.file_)
-        print(sample_file.sample_file_type)
+    for sample_file_group in util.grouper(imicrobe_db_session.query(models.Sample_file).limit(10), n=100):
 
         with irods.irods_session_manager() as irods_session:
-            if irods.irods_data_object_exists(irods_session=irods_session, target_path=sample_file.file_):
-                pass
-            else:
-                print('found "{}" in table sample_file but not in /iplant data store')
+            for sample_file in sample_file_group:
+                sample_file_i += 1
+                print('{}: "{}"'.format(sample_file_i, sample_file.sample_file_type))
+                if irods.irods_data_object_exists(irods_session=irods_session, target_path=sample_file.file_):
+                    pass
+                else:
+                    print('  found "{}" in table sample_file but not in /iplant data store')
