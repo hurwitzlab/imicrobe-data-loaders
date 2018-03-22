1fe18f60b87ca14a94f21d2e611c3e5a65b630b5
Joshua Lynch <jklynch@email.arizona.edu>
2018-03-22 14:45:29 -0400

    print some more information

--- imicrobe/load/sample_file_table/sync_with_iplant_data_store.py
+++ imicrobe/load/sample_file_table/sync_with_iplant_data_store.py
@@ -9,4 +9,6 @@ from orminator import session_manager_from_db_uri
 with session_manager_from_db_uri(db_uri=os.environ['IMICROBE_DB_URI']) as imicrobe_db_session:
     for sample_file in imicrobe_db_session.query(models.Sample_file).all():
         print(dir(sample_file))
+        print(sample_file.file_)
+        print(sample_file.sample_file_type)
         break
