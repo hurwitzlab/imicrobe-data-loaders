988a0d04beb43ffc4c51c14df22bcdd9cb5e5fdf
Joshua Lynch <jklynch@email.arizona.edu>
2018-03-22 14:23:46 -0400

    initial commit

---
+++ imicrobe/load/sample_file_table/sync_with_iplant_data_store.py
@@ -0,0 +1,12 @@
+import os
+
+import imicrobe.load.models as models
+from orminator import session_manager_from_db_uri
+
+
+# check every row in table sample_file
+
+with session_manager_from_db_uri(db_uri=os.environ['IMICROBE_DB_URI']) as imicrobe_db_session:
+    for sample_file in imicrobe_db_session.query(models.Sample_file).all():
+        print(sample_file)
+        break
