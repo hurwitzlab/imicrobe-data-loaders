4915c066736cfaa85dfc2329c2f24ddd42a022fe
Joshua Lynch <jklynch@email.arizona.edu>
2018-03-22 15:08:42 -0400

    quit on the first sample file missing from the /iplant data store

--- imicrobe/load/sample_file_table/sync_with_iplant_data_store.py
+++ imicrobe/load/sample_file_table/sync_with_iplant_data_store.py
@@ -9,7 +9,7 @@ from orminator import session_manager_from_db_uri
 # check every row in table sample_file
 sample_file_i = 0
 with session_manager_from_db_uri(db_uri=os.environ['IMICROBE_DB_URI']) as imicrobe_db_session:
-    for sample_file_group in util.grouper(imicrobe_db_session.query(models.Sample_file).limit(10), n=100):
+    for sample_file_group in util.grouper(imicrobe_db_session.query(models.Sample_file).all(), n=100):
 
         with irods.irods_session_manager() as irods_session:
             for sample_file in [f for f in sample_file_group if f is not None]:
@@ -19,3 +19,4 @@ with session_manager_from_db_uri(db_uri=os.environ['IMICROBE_DB_URI']) as imicro
                     pass
                 else:
                     print('  found "{}" in table sample_file but not in /iplant data store'.format(sample_file.file_))
+                    quit()
