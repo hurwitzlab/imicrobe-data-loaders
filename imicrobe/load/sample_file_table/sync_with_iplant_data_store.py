eb50e39a9fd28b455797028c247de76ffe2649d0
Joshua Lynch <jklynch@email.arizona.edu>
2018-03-22 15:12:59 -0400

    check all rows in sample_file table

--- imicrobe/load/sample_file_table/sync_with_iplant_data_store.py
+++ imicrobe/load/sample_file_table/sync_with_iplant_data_store.py
@@ -14,9 +14,9 @@ with session_manager_from_db_uri(db_uri=os.environ['IMICROBE_DB_URI']) as imicro
         with irods.irods_session_manager() as irods_session:
             for sample_file in [f for f in sample_file_group if f is not None]:
                 sample_file_i += 1
-                print('{}: "{}"'.format(sample_file_i, sample_file.file_))
+                #print('{}: "{}"'.format(sample_file_i, sample_file.file_))
                 if irods.irods_data_object_exists(irods_session=irods_session, target_path=sample_file.file_):
                     pass
                 else:
-                    print('  found "{}" in table sample_file but not in /iplant data store'.format(sample_file.file_))
-                    quit()
+                    print('{} found "{}" in table sample_file but not in /iplant data store'.format(sample_file_i, sample_file.file_))
+                    #quit()
