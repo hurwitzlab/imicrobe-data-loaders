3a0dd4314edd605b69f2772c8eae68d36c558acd
Joshua Lynch <jklynch@email.arizona.edu>
2018-03-22 15:05:55 -0400

    fix bugs

--- imicrobe/load/sample_file_table/sync_with_iplant_data_store.py
+++ imicrobe/load/sample_file_table/sync_with_iplant_data_store.py
@@ -12,10 +12,10 @@ with session_manager_from_db_uri(db_uri=os.environ['IMICROBE_DB_URI']) as imicro
     for sample_file_group in util.grouper(imicrobe_db_session.query(models.Sample_file).limit(10), n=100):
 
         with irods.irods_session_manager() as irods_session:
-            for sample_file in sample_file_group:
+            for sample_file in [f for f in sample_file_group if f is not None]:
                 sample_file_i += 1
-                print('{}: "{}"'.format(sample_file_i, sample_file.sample_file_type))
+                print('{}: "{}"'.format(sample_file_i, sample_file.file_))
                 if irods.irods_data_object_exists(irods_session=irods_session, target_path=sample_file.file_):
                     pass
                 else:
-                    print('  found "{}" in table sample_file but not in /iplant data store')
+                    print('  found "{}" in table sample_file but not in /iplant data store'.format(sample_file.file_))
