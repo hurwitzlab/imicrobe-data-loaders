53dc3abcacbcaf3270d5c1b502c4479650f43751
Joshua Lynch <jklynch@email.arizona.edu>
2018-03-22 22:01:42 -0400

    print table of projects and samples

--- imicrobe/load/sample_file_table/find_iplant_objects_not_in_sample_file_table.py
+++ imicrobe/load/sample_file_table/find_iplant_objects_not_in_sample_file_table.py
@@ -1,5 +1,9 @@
+import pprint
+
 import imicrobe.util.irods as irods
 
 
 with irods.irods_session_manager() as irods_session:
     t = irods.get_project_sample_collection_paths(sample_limit=100)
+
+print(pprint.pformat(t))
