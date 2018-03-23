82cce8bb0dffc7459acafcd92cb22e2e8255a896
Joshua Lynch <jklynch@email.arizona.edu>
2018-03-22 22:19:30 -0400

    more output

--- imicrobe/load/sample_file_table/find_iplant_objects_not_in_sample_file_table.py
+++ imicrobe/load/sample_file_table/find_iplant_objects_not_in_sample_file_table.py
@@ -11,10 +11,11 @@ for project, samples in irods.get_project_sample_collection_paths(sample_limit=1
     for sample in samples:
         for parent_collection, child_collections, data_objects in irods.walk(sample):
             for data_object in data_objects:
+                print('searching table sample_file for "{}"'.format(data_object.path))
                 with session_manager_from_db_uri(db_uri=os.environ['IMICROBE_DB_URI']) as imicrobe_db_session:
                     s = imicrobe_db_session.query(
                         models.Sample_file).filter(
                             models.Sample_file.file_ == data_object.path).one_or_none()
 
                     if s is None:
-                        print('failed to find "{}" in imicrobe sample_file table'.format(data_object.path))
+                        print('  failed to find "{}" in imicrobe sample_file table'.format(data_object.path))
