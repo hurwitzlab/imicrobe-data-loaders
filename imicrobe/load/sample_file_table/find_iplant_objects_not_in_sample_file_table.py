99f9378ccb32eddcaa7da283a543f0b8e8b9146d
Joshua Lynch <jklynch@email.arizona.edu>
2018-03-22 22:15:42 -0400

    check the imicrobe database for sample data objects

--- imicrobe/load/sample_file_table/find_iplant_objects_not_in_sample_file_table.py
+++ imicrobe/load/sample_file_table/find_iplant_objects_not_in_sample_file_table.py
@@ -1,9 +1,19 @@
 import pprint
 
+import imicrobe.models as models
 import imicrobe.util.irods as irods
+from orminator import session_manager_from_db_uri
 
 
-with irods.irods_session_manager() as irods_session:
-    t = irods.get_project_sample_collection_paths(sample_limit=100)
+for project, samples in irods.get_project_sample_collection_paths(sample_limit=100):
+    print('project "{}"'.format(project))
+    for sample in samples:
+        for parent_collection, child_collections, data_objects in walk(sample):
+            for data_object in data_objects:
+                with session_manager_from_db_uri(db_uri=os.environ['IMICROBE_DB_URI']) as imicrobe_db_session:
+                    s = imicrobe_db_session.query(
+                        models.Sample_file).filter(
+                            models.Sample_file.file_ == data_object.path).one_or_none()
 
-print(pprint.pformat(t))
+                    if s is None:
+                        print('failed to find "{}" in imicrobe sample_file table'.format(data_object.path))
