b6da4f537442c814aaec8476fef991b00efc31a1
Joshua Lynch <jklynch@email.arizona.edu>
2018-03-22 22:43:58 -0400

    parse project id and sample id from the data object path

--- imicrobe/load/sample_file_table/find_iplant_objects_not_in_sample_file_table.py
+++ imicrobe/load/sample_file_table/find_iplant_objects_not_in_sample_file_table.py
@@ -1,22 +1,35 @@
 import os
-import pprint
+import re
 
 import imicrobe.models as models
 import imicrobe.util.irods as irods
 from orminator import session_manager_from_db_uri
 
 
-for project, samples in irods.get_project_sample_collection_paths(sample_limit=100).items():
-    print('project "{}"'.format(project))
-    print('failed to find:')
-    for sample in samples:
-        for parent_collection, child_collections, data_objects in irods.walk(sample):
-            for data_object in data_objects:
-                #print('searching table sample_file for "{}"'.format(data_object.path))
-                with session_manager_from_db_uri(db_uri=os.environ['IMICROBE_DB_URI']) as imicrobe_db_session:
-                    s = imicrobe_db_session.query(
-                        models.Sample_file).filter(
-                            models.Sample_file.file_ == data_object.path).one_or_none()
+def find_missing_sample_files():
 
-                    if s is None:
-                        print('  {}'.format(data_object.path))
+    for project, samples in irods.get_project_sample_collection_paths(sample_limit=100).items():
+        print('project "{}"'.format(project))
+        print('failed to find:')
+        for sample in samples:
+            for parent_collection, child_collections, data_objects in irods.walk(sample):
+                for data_object in data_objects:
+                    #print('searching table sample_file for "{}"'.format(data_object.path))
+                    with session_manager_from_db_uri(db_uri=os.environ['IMICROBE_DB_URI']) as imicrobe_db_session:
+                        s = imicrobe_db_session.query(
+                            models.Sample_file).filter(
+                                models.Sample_file.file_ == data_object.path).one_or_none()
+
+                        if s is None:
+                            yield data_object.path
+
+
+path_pattern = re.compile(r'projects/(?P<project_id>\d+)/samples/(?P<sample_id>\d+)')
+for missing_sample_file in find_missing_sample_files():
+    print('  {}'.format(missing_sample_file))
+    path_match = path_pattern.search(missing_sample_file)
+    if path_match is None:
+        raise Exception()
+    else:
+        print('    project: {}'.format(path_match.group('project_id')))
+        print('    sample: {}'.format(path_match.group('sample_id')))
