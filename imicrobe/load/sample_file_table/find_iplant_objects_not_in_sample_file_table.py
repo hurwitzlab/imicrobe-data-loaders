b6572277d36b55a0f058afee62511fc87f6d79d7
Joshua Lynch <jklynch@email.arizona.edu>
2018-03-24 00:45:00 -0400

    add command line argument --sample-limit

--- imicrobe/load/sample_file_table/find_iplant_objects_not_in_sample_file_table.py
+++ imicrobe/load/sample_file_table/find_iplant_objects_not_in_sample_file_table.py
@@ -1,14 +1,24 @@
+import argparse
 import os
 import re
+import time
 
 import imicrobe.models as models
 import imicrobe.util.irods as irods
 from orminator import session_manager_from_db_uri
 
 
-def find_missing_sample_files():
+def get_args():
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument('--sample-limit', type=int, default=None, required=False)
+    args = arg_parser.parse_args()
+    print(args)
+    return args
 
-    for project, samples in irods.get_project_sample_collection_paths(sample_limit=100).items():
+
+def find_missing_sample_files(sample_limit=100):
+
+    for project, samples in irods.get_project_sample_collection_paths(sample_limit=sample_limit).items():
         print('project "{}"'.format(project))
         print('failed to find:')
         for sample in samples:
@@ -18,14 +28,25 @@ def find_missing_sample_files():
                     with session_manager_from_db_uri(db_uri=os.environ['IMICROBE_DB_URI']) as imicrobe_db_session:
                         s = imicrobe_db_session.query(
                             models.Sample_file).filter(
-                                models.Sample_file.file_ == data_object.path).one_or_none()
-
-                        if s is None:
+                                models.Sample_file.file_ == data_object.path).all()
+                        if len(s) == 0:
+                            # this path is not in the database
+                            print('  {}'.format(data_object.path))
                             yield data_object.path
+                        elif len(s) == 1:
+                            # this path is in the database
+                            pass
+                        else:
+                            # this path is in the database more than once
+                            print('WARNING: "{}" was found {} times'.format(data_object.path. len(s)))
+# why?
+# collection "/iplant/home/shared/imicrobe/projects/95/samples" does not exist
 
-
+args = get_args()
 path_pattern = re.compile(r'projects/(?P<project_id>\d+)/samples/(?P<sample_id>\d+)')
-for missing_sample_file in find_missing_sample_files():
+t0 = time.time()
+new_sample_file_count = 0
+for missing_sample_file in find_missing_sample_files(sample_limit=args.sample_limit):
     print('  {}'.format(missing_sample_file))
     path_match = path_pattern.search(missing_sample_file)
     if path_match is None:
@@ -35,13 +56,26 @@ for missing_sample_file in find_missing_sample_files():
         print('    sample: {}'.format(path_match.group('sample_id')))
         sample_id = int(path_match.group('sample_id'))
         with session_manager_from_db_uri(os.environ['IMICROBE_DB_URI']) as imicrobe_db_session:
+            sample_file_type_meta_id = imicrobe_db_session.query(
+                models.Sample_file_type).filter(
+                    models.Sample_file_type.type_ == 'Meta').one().sample_file_type_id
+
             sample = imicrobe_db_session.query(models.Sample).filter(models.Sample.sample_id == sample_id).one()
-            print('    sample has {} sample files'.format(len(sample.sample_file_list)))
-            sample_file = models.Sample_file()
-            sample_file.file_ = missing_sample_file
+            #print('    sample has {} sample files'.format(len(sample.sample_file_list)))
+
+            sample_file = models.Sample_file(
+                file_=missing_sample_file,
+                sample_id=sample.sample_id,
+                sample_file_type_id=sample_file_type_meta_id)
+            imicrobe_db_session.add(sample_file)
+            #sample_file.file_ = missing_sample_file
             sample_file.sample_file_type = imicrobe_db_session.query(
                 models.Sample_file_type).filter(
                     models.Sample_file_type.type_ == 'Meta').one()
             sample.sample_file_list.append(sample_file)
+            new_sample_file_count += 1
+
     else:
         print('  not a JSON file: {}'.format(missing_sample_file))
+
+print('inserted {} sample_file rows in {:5.2f}s'.format(new_sample_file_count, time.time()-t0))
